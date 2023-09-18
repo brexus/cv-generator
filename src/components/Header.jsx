6015e695a5f63a0314e50c83b9fb5ee5d12b50be
brexus <92671269+brexus@users.noreply.github.com>
2023-09-18 11:50:57 +0200

    add fullReset, printPDF and loadExamplePreview and connect them to the buttons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,63 +1,57 @@
 import "../styles/Header.css";
 
-function Button({text, funct}) {
 
-    return (
-        <button type="text" onClick={funct}>
-            {text}
-        </button>
-    );
-};
+export default function Header({dataCV, setDataCV, setInitialDataCV, skillsFF, educationDataFF, workExperienceFF}) {
 
-export default function Header({dataCV, setDataCV}) {
+    const loadExamplePreview = () => {
+        
+        setDataCV({
+            personalData: {
+                fullName: "Anna Kowalska",
+                jobTitle: "Księgowa",
+                email: "a.kowalska74@gmail.com",
+                phone: "666777888",
+                address: "ul. Nowatorska 74A, Warszawa",
+                dateOB: "21.07.1974r."
+            },
+    
+            educationData: [
+                educationDataFF(0, "Ekonomia", "UMCS Lublin", "1996-1999"),
+                educationDataFF(1, "Human Resources", "SGH", "2002-2006")
+            ],
+    
+            workExperienceData: [
+                workExperienceFF(0, "Microsoft", "ul.Racławkowska 12/65", "Księgowość", "2000-2020"),
+                workExperienceFF(1, "Google", "ul.Nowatorska", "Księgowość", "2020-now")
+            ],
+    
+            skillsData: [
+                skillsFF(0, "Obsługa programów MS Office (Excel, Word, PowerPoint itp.)"),
+                skillsFF(1, "Obsługa nowoczesnych programów firmy Adobe do zarządzania kosztami firmy"),
+                skillsFF(2, "Znajmość języka angielskiego na poziomie C1")
+            ]
+        })
 
-    // const loadExamplePreview = () => {
-    //     setFullNameState("Anna Kowalska");
-    //     setJobTitleState("Księgowa");
-    //     setEmailState("a.kowalska74@gmail.com");
-    //     setPhoneState("666777888");
-    //     setAddressState("ul. Nowatorska 74A, Warszawa");
-    //     setDateOfBirthState("21.07.1974r.");
-    //     setFieldOfStudyState("Ekonomia");
-    //     setNameOfUniversityState("UMCS Lublin");
-    //     setDurationEducationState("1996-1999");
-    //     setCompanyNameState("Microsoft");
-    //     setCompanyAddressState("ul.Racławkowska 12/65");
-    //     setJobTitleWorkExperienceState("Księgowa");
-    //     setDurationWorkExperienceState("2000-2023");
-    //     setTitleSkillsState("Obsługa pakietu Microsoft Office (Excel, Word itp.)");
-    // };
+    };
 
-    // const fullReset = () => {
-    //     setFullNameState("");
-    //     setJobTitleState("");
-    //     setEmailState("");
-    //     setPhoneState("");
-    //     setAddressState("");
-    //     setDateOfBirthState("");
-    //     setFieldOfStudyState("");
-    //     setNameOfUniversityState("");
-    //     setDurationEducationState("");
-    //     setCompanyNameState("");
-    //     setCompanyAddressState("");
-    //     setJobTitleWorkExperienceState("");
-    //     setDurationWorkExperienceState("");
-    //     setTitleSkillsState("");
-    // };
+    const fullReset = () => {
+        setDataCV(setInitialDataCV);
+    };
 
-    // const printPDF = () => {
-    //     const preview = document.getElementById("sheet");
-    //     console.log(preview);
-    //     window.print();
-    // };
+    const printPDF = () => {
+        const preview = document.getElementById("sheet");
+        console.log(preview);
+        window.print();
+    };
 
     return (
         <div className="component" id="header">
             <h1>CV Generator</h1>
             <div id="header-buttons">
-                <Button text="Save" />
-                <Button text="Example" />
-                <Button text="Reset" />
+                <button onClick={printPDF}>Save</button>
+                <button onClick={loadExamplePreview}>Example</button>
+                <button onClick={fullReset}>Reset</button>
+
             </div>
         </div>
     );
